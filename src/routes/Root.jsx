02754c6029f570f4383ccf91a81690c91b7dc781
Luan Tavares (tavaresl) <tavaresl@coventry.ac.uk>
2023-04-26 14:33:12 +0100

    removed buggy parallax effect and unused useEffect

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,26 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Nav from "../components/Nav/Nav";
 import { Outlet, useLocation } from "react-router-dom";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
 import { Background } from "../components/Background/Background";
 export default function Root() {
   const [pageNum, setPageNum] = useState(1.2);
 
   const location = useLocation();
-
-  useEffect(() => {
-    console.log(pageNum);
-    if (location.pathname === "/") {
-      setPageNum(1.2);
-    } else if (
-      location.pathname === "/society" ||
-      location.pathname === "/contact"
-    ) {
-      setPageNum(1);
-    } else {
-      setPageNum(1);
-    }
-  }, [location.pathname]);
 
   return (
     <div>
