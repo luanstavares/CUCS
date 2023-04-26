5b9d7fdd421af58f19253944224df8df541cb12b
Luan Tavares (tavaresl) <tavaresl@coventry.ac.uk>
2023-04-26 14:31:40 +0100

    css modules to a predefined material ui component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import * as styles from "./Home.module.css";
 import { Button, Typography, useTheme, Box } from "@mui/material";
 import { ArrowForward } from "@mui/icons-material";
-import { Background } from "../../components/Background/Background";
-import { ParallaxLayer } from "@react-spring/parallax";
+import { Card } from "../../components/Card/Card";
 
 export default function Home() {
   const theme = useTheme();
@@ -34,34 +33,32 @@ export default function Home() {
             Join Us
           </Button>
 
-          <div className={styles.card}>
-            <div className="cardContent">
-              <Typography
-                textAlign={"center"}
-                fontWeight={700}
-                variant="h5">
-                Our Goal
-              </Typography>
-              <Typography
-                fontWeight={500}
-                paddingTop={"5rem"}
-                textAlign={"justify"}
-                variant="subtitle1">
-                “Lorem ipsum dolor sit amet consectetur. Dictum diam sagittis
-                nulla placerat urna. Suspendisse non imperdiet malesuada mauris
-                risus massa tincidunt quis sodales. Cursus ac pretium augue
-                lacus integer. Mattis mauris sit egestas nibh habitant
-                condimentum in. Duis id ipsum consequat.”
-              </Typography>
-              <Typography
-                paddingTop={"5rem"}
-                textAlign={"right"}
-                fontWeight={500}
-                variant="h6">
-                CS President.
-              </Typography>
-            </div>
-          </div>
+          <Card>
+            <Typography
+              textAlign={"center"}
+              fontWeight={700}
+              variant="h5">
+              Our Goal
+            </Typography>
+            <Typography
+              fontWeight={500}
+              paddingTop={"5rem"}
+              textAlign={"justify"}
+              variant="subtitle1">
+              “Lorem ipsum dolor sit amet consectetur. Dictum diam sagittis
+              nulla placerat urna. Suspendisse non imperdiet malesuada mauris
+              risus massa tincidunt quis sodales. Cursus ac pretium augue lacus
+              integer. Mattis mauris sit egestas nibh habitant condimentum in.
+              Duis id ipsum consequat.”
+            </Typography>
+            <Typography
+              paddingTop={"5rem"}
+              textAlign={"right"}
+              fontWeight={500}
+              variant="h6">
+              CS President.
+            </Typography>
+          </Card>
         </Box>
       </div>
     </>
