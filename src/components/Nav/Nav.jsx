209e877d1471081a053abe035dfec0444c210108
Luan Tavares (tavaresl) <tavaresl@coventry.ac.uk>
2023-04-26 14:28:43 +0100

    created a card component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,6 @@ import * as styles from "./Nav.module.css";
 import { ListLinks } from "./ListLinks";
 import Logo from "../Logo/Logo";
 import SocialMediaLinks from "./SocialMediaLinks";
-import { ParallaxLayer } from "@react-spring/parallax";
 
 export default function Nav() {
   return (
