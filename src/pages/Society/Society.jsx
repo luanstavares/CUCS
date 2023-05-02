ad0bb133677b54c498abeb72076564a3a8a49629
Luan Tavares (tavaresl) <tavaresl@coventry.ac.uk>
2023-05-02 21:35:52 +0100

    changing descriptions for prod

--- src/pages/Society/Society.jsx
+++ src/pages/Society/Society.jsx
@@ -10,70 +10,70 @@ export default function Society() {
   const matches = useMediaQuery(theme.breakpoints.up("sm"));
   const committeeItems = [
     {
-      position: "President",
+      position: "President 😎",
       name: "Vanshul Kumar",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "The President plays a critical role in ensuring the success of the society, providing leadership, guidance, and support to members and helping to advance the society's mission.",
       image: "vanshul.png",
     },
     {
-      position: "Inclusion Officer",
+      position: "Inclusion Officer 🤝",
       name: "John",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "The Inclusion Officer works to ensure that the society is a friendly and inclusive environment for all members by developing policies, offering education, and promoting diversity within the society environment.",
       image: "john.png",
     },
     {
-      position: "Social Media Coordinator",
+      position: "Social Media Coordinator 📱",
       name: "Penny Mwaura",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "The Social Media Coordinator plays an important role in promoting the society and its activities. They are responsible for creating and sharing content that is informative, engaging, and visually appealing.",
       image: "penny.png",
     },
     {
-      position: "Tresurer",
+      position: "Tresurer 💰",
       name: "Bandhan Bhaliya",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "The Treasurer is a member of the executive board of the society. They are responsible for the financial management of the society, including budgeting, fundraising, and accounting.",
       image: "bandhan.png",
     },
   ];
 
   const cardItems = [
     {
-      title: "LeetCode",
+      title: "LeetCode 📝",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "LeetCode is a platform that provides a collection of coding problems and challenges that can be used to improve programming skills and prepare for technical interviews.",
       image: "leetcode.png",
     },
     {
-      title: "Tech Talk",
+      title: "Tech Talk 🎙️",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "Tech Talks is an event that provides an opportunity for students to come together and learn about new and emerging technologies, tools, and programming languages.",
       image: "faang.jpeg",
     },
     {
-      title: "Coding Support",
+      title: "Coding Support 🤝",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "No, we don't have all the answers, but we are code lovers eager to promote these sessions as a wonderful method to obtain assistance with your coding issues.",
       image: "codingSupport.webp",
     },
     {
-      title: "Chess Matches",
+      title: "Chess Matches ♟️",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "Chess sessions are events that provides an opportunity for students and faculty members to come together and play chess in a friendly and competitive setting making it a popular pastime.",
       image: "chess.jpeg",
     },
     {
-      title: "Discord",
+      title: "Discord 💻",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "Discord is a powerful tool for virtual collaboration and can be used to facilitate discussions, share resources, and build a sense of community among members of the coding society.",
       image: "discord.png",
     },
     {
-      title: "Socials",
+      title: "Socials 🍻",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "Social sessions held by the society are events that provide an opportunity for members of the society to come together and engage in social activities outside of coding and programming.",
       image: "social.jpeg",
     },
   ];
